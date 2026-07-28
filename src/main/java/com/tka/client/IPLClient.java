b750421b314daf7dced8c99fe334c5f24f2ebc2a
Nikita Nalkol <nalkolnikita02@gmail.com>
2026-07-28 19:21:43 +0530

    Implemented getOnePlayer method

--- src/main/java/com/tka/client/IPLClient.java
+++ src/main/java/com/tka/client/IPLClient.java
@@ -1,10 +1,18 @@
 package com.tka.client;
 
+
+import com.tka.dao.IPLDao;
+import com.tka.entity.Player;
+
 public class IPLClient {
 
 	public static void main(String[] args) {
 
+		IPLDao dao = new IPLDao();
 
+	    Player player = dao.getOnePlayer(1);
+
+	    System.out.println(player);
 	
 	}
 
