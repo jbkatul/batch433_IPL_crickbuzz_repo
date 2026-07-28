06aa43feb2c126b05319e959c3f38f9643f4bc6b
Yash230765 <yashchavan073@gmail.com>
2026-07-28 19:01:27 +0530

    Added player Pojo class

--- src/main/java/com/tka/dao/DaoInterfaces.java
+++ src/main/java/com/tka/dao/DaoInterfaces.java
@@ -7,9 +7,11 @@ import com.tka.entity.Player;
 public interface DaoInterfaces {
 	
 	public abstract Player getOnePlayer(int pk);
+	public abstract List<Player> getallPlayer1(String team);
 	public abstract List<Player> getallPlayer();
 	public abstract Player InsertOnePlayer(Player obj);
 	public abstract Player UpdateOnePlayer(Player obj);
 	public abstract Boolean DeleteOnePlayer(int pk);
+	
 
 }
--- src/main/java/com/tka/entity/Player.java
+++ src/main/java/com/tka/entity/Player.java
@@ -1,13 +1,75 @@
 package com.tka.entity;
 
 public class Player {
-	
-	private int p_id;
-	private int jn;
-	private String p_name;
-	private int runs;
-	private int wickets;
-	private String tname;
-	private String specialisation;
 
+    private int p_id;
+    private int jn;
+    private String p_name;
+    private int runs;
+    private int wickets;
+    private String tname;
+    private String specialisation;
+
+    public int getP_id() {
+        return p_id;
+    }
+
+    public void setP_id(int p_id) {
+        this.p_id = p_id;
+    }
+
+    public int getJn() {
+        return jn;
+    }
+
+    public void setJn(int jn) {
+        this.jn = jn;
+    }
+
+    public String getP_name() {
+        return p_name;
+    }
+
+    public void setP_name(String p_name) {
+        this.p_name = p_name;
+    }
+
+    public int getRuns() {
+        return runs;
+    }
+
+    public void setRuns(int runs) {
+        this.runs = runs;
+    }
+
+    public int getWickets() {
+        return wickets;
+    }
+
+    public void setWickets(int wickets) {
+        this.wickets = wickets;
+    }
+
+    public String getTname() {
+        return tname;
+    }
+
+    public void setTname(String tname) {
+        this.tname = tname;
+    }
+
+    public String getSpecialisation() {
+        return specialisation;
+    }
+
+    public void setSpecialisation(String specialisation) {
+        this.specialisation = specialisation;
+    }
+
+    @Override
+    public String toString() {
+        return "Player [p_id=" + p_id + ", jn=" + jn + ", p_name=" + p_name
+                + ", runs=" + runs + ", wickets=" + wickets
+                + ", tname=" + tname + ", specialisation=" + specialisation + "]";
+    }
 }
