a7beda686e07a97d0e63ef5c15d862bca24e6791
Kanchan_Bade <kanchanbade07@gmail.com>
2026-07-29 14:28:08 +0530

    Added IPL player dataset and display all players feature

--- src/main/java/com/tka/client/IPLClient.java
+++ src/main/java/com/tka/client/IPLClient.java
@@ -1,10 +1,22 @@
 package com.tka.client;
+import java.util.List;
+
+import com.tka.dao.IPLDao;
+import com.tka.entity.Player;
 
 public class IPLClient {
 
 	public static void main(String[] args) {
 
+		IPLDao dao = new IPLDao();
 
+		List<Player> players = dao.getallPlayer();
+
+		System.out.println("------ ALL PLAYERS ------");
+
+		for (Player p : players) {
+			System.out.println(p);
+		}
 	
 	}
 
