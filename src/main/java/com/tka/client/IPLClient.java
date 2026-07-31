7131eaf2e522e53a02d3b740433d2e5254200e9b
Kanchan_Bade <kanchanbade07@gmail.com>
2026-07-31 10:15:28 +0530

    Added menu-driven user interface for IPL Hibernate project

--- src/main/java/com/tka/client/IPLClient.java
+++ src/main/java/com/tka/client/IPLClient.java
@@ -1,23 +1,103 @@
 package com.tka.client;
+
 import java.util.List;
+import java.util.Scanner;
 
 import com.tka.dao.IPLDao;
 import com.tka.entity.Player;
-
 
 public class IPLClient {
 
 	public static void main(String[] args) {
 
 		IPLDao dao = new IPLDao();
+		Scanner sc = new Scanner(System.in);
 
-		List<Player> players = dao.getallPlayer();
+		int choice;
 
-		System.out.println("------ ALL PLAYERS ------");
+		do {
+			System.out.println("\n*************** IPL MENU ***************");
+			System.out.println("1. Show All Players");
+			System.out.println("2. Show Player By ID");
+			System.out.println("3. Show Players By Team");
+			System.out.println("4. Show Batsmen By Team");
+			System.out.println("5. Show Bowlers By Team");
+			System.out.println("6. Show Allrounders By Team");
+			System.out.println("7. Search Player By Starting Letter");
+			System.out.println("8. Exit");
+			System.out.print("Enter your choice: ");
 
-		for(Player p : players){
-			System.out.println(p);
-		}
+			choice = sc.nextInt();
+			sc.nextLine(); // consume newline
+
+			switch (choice) {
+
+				case 1:
+					List<Player> allPlayers = dao.getallPlayer();
+					allPlayers.forEach(System.out::println);
+					break;
+
+				case 2:
+					System.out.print("Enter Player ID: ");
+					int id = sc.nextInt();
+					Player p = dao.getOnePlayer(id);
+
+					if (p != null)
+						System.out.println(p);
+					else
+						System.out.println("Player not found");
+					break;
+
+				case 3:
+					System.out.print("Enter Team Name: ");
+					String team = sc.nextLine();
+
+					List<Player> teamPlayers = dao.getallPlayer(team);
+					teamPlayers.forEach(System.out::println);
+					break;
+
+				case 4:
+					System.out.print("Enter Team Name: ");
+					team = sc.nextLine();
+
+					List<Player> batsmen = dao.getallBatsman(team);
+					batsmen.forEach(System.out::println);
+					break;
+
+				case 5:
+					System.out.print("Enter Team Name: ");
+					team = sc.nextLine();
+
+					List<Player> bowlers = dao.getallBowlers(team);
+					bowlers.forEach(System.out::println);
+					break;
+
+				case 6:
+					System.out.print("Enter Team Name: ");
+					team = sc.nextLine();
+
+					List<Player> allrounders = dao.getallAllrounders(team);
+					allrounders.forEach(System.out::println);
+					break;
+
+				case 7:
+					System.out.print("Enter Starting Letter: ");
+					String letter = sc.nextLine();
+
+					List<Player> names = dao.getplayers_name_starts_with(letter);
+					names.forEach(System.out::println);
+					break;
+
+				case 8:
+					System.out.println("Thank you! Exiting application...");
+					break;
+
+				default:
+					System.out.println("Invalid choice! Try again.");
+			}
+
+		} while (choice != 8);
+
+		sc.close();
 	}
-
 }
--- src/main/java/com/tka/dao/IPLDao.java
+++ src/main/java/com/tka/dao/IPLDao.java
@@ -4,13 +4,23 @@ import java.util.List;
 import com.tka.entity.Player;
 import org.hibernate.Session;
 import com.tka.utility.HibernateUtil;
+import org.hibernate.Transaction;
+
 
 public class IPLDao implements DaoInterfaces{
 
 	@Override
 	public Player getOnePlayer(int pk) {
-		// TODO Auto-generated method stub
-		return null;
+
+		Session session = HibernateUtil
+				.getSessionFactory()
+				.openSession();
+
+		Player player = session.get(Player.class, pk);
+
+		session.close();
+
+		return player;
 	}
 
 	@Override
@@ -29,22 +39,151 @@ public class IPLDao implements DaoInterfaces{
 		return list;
 	}
 
+	public List<Player> getallPlayer(String team) {
+
+		Session session = HibernateUtil
+				.getSessionFactory()
+				.openSession();
+
+		List<Player> list = session
+				.createQuery(
+						"from Player where tname = :team",
+						Player.class)
+				.setParameter("team", team)
+				.list();
+
+		session.close();
+
+		return list;
+	}
+
+	public List<Player> getallBatsman(String team) {
+
+		Session session = HibernateUtil
+				.getSessionFactory()
+				.openSession();
+
+		List<Player> list = session
+				.createQuery(
+						"from Player where tname = :team and specialisation = 'Batsman'",
+						Player.class)
+				.setParameter("team", team)
+				.list();
+
+		session.close();
+
+		return list;
+	}
+
+	public List<Player> getallBowlers(String team) {
+
+		Session session = HibernateUtil
+				.getSessionFactory()
+				.openSession();
+
+		List<Player> list = session
+				.createQuery(
+						"from Player where tname = :team and specialisation = 'Bowler'",
+						Player.class)
+				.setParameter("team", team)
+				.list();
+
+		session.close();
+
+		return list;
+	}
+
+	public List<Player> getallAllrounders(String team) {
+
+		Session session = HibernateUtil
+				.getSessionFactory()
+				.openSession();
+
+		List<Player> list = session
+				.createQuery(
+						"from Player where tname = :team and specialisation = 'Allrounder'",
+						Player.class)
+				.setParameter("team", team)
+				.list();
+
+		session.close();
+
+		return list;
+	}
+
+	public List<Player> getplayers_name_starts_with(String name) {
+
+		Session session = HibernateUtil
+				.getSessionFactory()
+				.openSession();
+
+		List<Player> list = session
+				.createQuery(
+						"from Player where p_name like :name",
+						Player.class)
+				.setParameter("name", name + "%")
+				.list();
+
+		session.close();
+
+		return list;
+	}
+
 	@Override
 	public Player InsertOnePlayer(Player obj) {
-		// TODO Auto-generated method stub
-		return null;
+
+		Session session = HibernateUtil
+				.getSessionFactory()
+				.openSession();
+
+		Transaction tx = session.beginTransaction();
+
+		session.save(obj);
+
+		tx.commit();
+		session.close();
+
+		return obj;
 	}
 
 	@Override
 	public Player UpdateOnePlayer(Player obj) {
-		// TODO Auto-generated method stub
-		return null;
+
+		Session session = HibernateUtil
+				.getSessionFactory()
+				.openSession();
+
+		Transaction tx = session.beginTransaction();
+
+		session.update(obj);
+
+		tx.commit();
+		session.close();
+
+		return obj;
 	}
 
 	@Override
 	public Boolean DeleteOnePlayer(int pk) {
-		// TODO Auto-generated method stub
-		return null;
+
+		Session session = HibernateUtil
+				.getSessionFactory()
+				.openSession();
+
+		Transaction tx = session.beginTransaction();
+
+		Player p = session.get(Player.class, pk);
+
+		if (p != null) {
+			session.delete(p);
+			tx.commit();
+			session.close();
+			return true;
+		}
+
+		tx.rollback();
+		session.close();
+		return false;
 	}
 
 }
